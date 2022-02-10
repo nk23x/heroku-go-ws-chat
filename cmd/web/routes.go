d1e2f103a6227e3cd4f362f83568f08704b865cc
SajjdManafi <sajadmanafi8@gmail.com>
2022-02-10 22:55:45 +0330

    add router

---
+++ cmd/web/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/SajjadManafi/ws-chat/internal/handlers"
+	"github.com/bmizerany/pat"
+)
+
+func routes() http.Handler {
+
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+
+	return mux
+
+}
