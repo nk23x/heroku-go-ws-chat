7d9766ff6a2844f4dd68c850472259b3fcd84ece
SajjdManafi <sajadmanafi8@gmail.com>
2022-02-10 22:54:58 +0330

    using jet in handlers and create page render function

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,35 @@
 package handlers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	"github.com/CloudyKit/jet/v6"
+)
 
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode(),
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func renderPage(w http.ResponseWriter, templateName string, data jet.VarMap) error {
+	view, err := views.GetTemplate(templateName)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
 }
