7925b72443b755fef27458fe90c1994d68fa440c
SajjdManafi <sajadmanafi8@gmail.com>
2022-02-11 16:50:42 +0330

    try to reconnect when server goes away in js

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,6 +14,9 @@ func routes() http.Handler {
 	mux.Get("/", http.HandlerFunc(handlers.Home))
 	mux.Get("/ws", http.HandlerFunc(handlers.WsEndPoint))
 
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+
 	return mux
 
 }
