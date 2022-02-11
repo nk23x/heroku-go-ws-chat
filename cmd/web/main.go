1bc2cd0ab66b79d68fb4a1517f5d7172d1d289d9
SajjdManafi <sajadmanafi8@gmail.com>
2022-02-11 18:27:52 +0330

    project ready to deploy (heroku)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/SajjadManafi/ws-chat/internal/handlers"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = "8080"
+	}
+
 	mux := routes()
 
 	log.Println("Starting channel listener...")
 	go handlers.ListenToWsChannel()
 
-	log.Println("Starting server on port 8080")
+	log.Printf("Starting server on port %s\n", port)
 
-	_ = http.ListenAndServe(":8080", mux)
+	_ = http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
 }
